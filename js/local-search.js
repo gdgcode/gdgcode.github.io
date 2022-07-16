aa21b66b85f231e6c60c2b33e158f20729e508cc
gdg <3480929051@qq.com>
2022-07-16 23:49:48 +0800

    Site updated: 2022-07-16 23:49:48

--- js/local-search.js
+++ js/local-search.js
@@ -276,3 +276,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+
+<!--崩溃欺骗-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         //$('[rel="icon"]').attr('href', "/images/favicon32.png");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         //$('[rel="icon"]').attr('href', "/images/favicon32.png");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
